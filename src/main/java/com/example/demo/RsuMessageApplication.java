3920027644ec3c248c3e5a303bca046ee669ea5b
lyndia <lyndia.libin@uberfusion.com>
2023-02-08 16:41:11 +0800

    Initial Commit

---
+++ src/main/java/com/example/demo/RsuMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsuMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsuMessageApplication.class, args);
+	}
+
+}
