b86b83788d7bafc8878b89745ae2c387dd04ab8b
vanaraen <vodkachoke65@gmail.com>
2023-02-09 10:09:57 +0000

    temporary commit

--- src/main/java/com/example/demo/RsuMessageApplication.java
+++ src/main/java/com/example/demo/RsuMessageApplication.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.concurrent.TimeUnit;
+import org.joda.time.DateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +12,20 @@ public class RsuMessageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RsuMessageApplication.class, args);
+		DateTime dt = new DateTime("2023-03-03") ;
+		dt.withTime(8, 0, 0, 0);
+		RSU(dt, 30, "DAYS");
+	}
+
+	static void RSU (DateTime startDate, Integer duration, String timeUnit) {
+		LocalDateTime startdt = startDate.toDate().toInstant().atZone(ZoneId.of("UTC")).toLocalDateTime();
+		System.out.println("start date : "+startdt);
+		LocalDateTime targetedEndDateByDuration = null;
+		if (timeUnit.equalsIgnoreCase(TimeUnit.DAYS.name())) {
+			targetedEndDateByDuration = startdt.plusDays(duration);
+		}
+
+		System.out.println("targetedEndDateByDuration: "+targetedEndDateByDuration);
 	}
 
 }
